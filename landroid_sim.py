f6c0515f3fa31cd7058d699a95212d01e9868d90
Malene <malene@trab.dk>
2022-07-14 13:17:09 +0000

    Dev code upload

--- landroid_sim.py
+++ landroid_sim.py
@@ -1,5 +1,11 @@
-    """Landroid Simulator
+"""
+Landroid Simulator
 
-    Used for testing and developing pyWorxcloud and Landroid_Cloud modules.
-    """
-    
+Used for testing and developing pyWorxcloud and Landroid_Cloud modules.
+"""
+
+from src import ServerBase
+
+if __name__ == "__main__":
+    server = ServerBase()
+    server.start()
---
+++ src/__init__.py
@@ -0,0 +1,34 @@
+"""Main program file."""
+
+import sys
+from typing import Any
+
+import web
+from cheroot.server import HTTPServer
+from cheroot.ssl.builtin import BuiltinSSLAdapter
+
+from .datasets import VendorName
+from .url_list import *
+
+# __all__ = ["ServerBase"]
+
+THIS_VENDOR = None
+
+
+class ServerBase:
+    """Base server class."""
+
+    __service: Any
+
+    def __init__(self, port: str = "88", vendor: VendorName = VendorName.WORX) -> Any:
+        """Init base server object."""
+        sys.argv.append(port)
+        THIS_VENDOR = vendor
+        self.__service = web.application(urls, globals())
+
+    def start(self) -> None:
+        """Starts the web service"""
+        HTTPServer.ssl_adapter = BuiltinSSLAdapter(
+            certificate="certs/server.crt", private_key="certs/server.key"
+        )
+        self.__service.run()
