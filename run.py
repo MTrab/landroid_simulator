60f4c9254e0551929472fd05ee508b0a3b3ce884
Malene <malene@trab.dk>
2022-07-16 14:28:17 +0000

    Update main run file

--- run.py
+++ run.py
@@ -4,27 +4,48 @@ Landroid Simulator
 Used for testing and developing pyWorxcloud and Landroid_Cloud modules.
 """
 
+import argparse
 import os
-from app.__version__ import app_author, app_license, app_version
-from app.tools.logger import Logger
+import app
 
 if __name__ == "__main__":
-    base_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "log")
-    print(
-        r"""
-    __                  __              _      __
-   / /  ___ _  ___  ___/ /  ____ ___   (_) ___/ /
-  / /__/ _ `/ / _ \/ _  /  / __// _ \ / / / _  /
- /_______,__ /_//_/\_,_/  /_/__ \___//___ \_,_/
-   / __/  (_)  __ _  __ __  / / ___ _ / /_ ___   ____
-  _\ \   / /  /  ' \/ // / / / / _ `// __// _ \ / __/
- /___/  /_/  /_/_/_/\_,_/ /_/  \_,_/ \__/ \___//_/
-    """
+    run_path = os.path.abspath(os.path.dirname(__file__))
+    parser = argparse.ArgumentParser(
+        description="Landroid Simulator commandline arguments",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    print("Version: " + str(app_version))
-    print("Author: " + str(app_author))
-    print("License: " + str(app_license))
+    parser.add_argument(
+        "-p",
+        "--port",
+        help="Set port number to be used for web interface",
+        default="88",
+        type=str,
+    )
+    parser.add_argument(
+        "-l",
+        "--log_level",
+        help="Set lowest loglevel",
+        default="info",
+        choices=["critical", "error", "warning", "info", "debug"],
+        type=str,
+    )
+    parser.add_argument(
+        "-b", "--log_backup", help="Number of log backups to keep", type=int
+    )
+    parser.add_argument(
+        "--log_path",
+        help="Path where log files will be saved",
+        type=str,
+        default=os.path.join(run_path, "log"),
+    )
+    parser.add_argument(
+        "--ip",
+        help="IP address to listen for requests",
+        type=str,
+        default="0.0.0.0",
+    )
 
-    log_debug = os.path.join(base_path, "landroid_simulator.log")
-    log_err = os.path.join(base_path, "landroid_simulator.log")
-    Logger.create_logger(log_debug, log_err, __package__)
+    config = vars(parser.parse_args())
+    config.update({"template_path": os.path.join(run_path, "app/templates")})
+
+    app.main(config)
